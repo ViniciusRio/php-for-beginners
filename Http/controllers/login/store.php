41bf02f59a019c3500e2978aab3940429162f695
Vinicius Rio <viniciustrave@gmail.com>
2023-04-23 17:24:42 -0300

    New folder and refactor form validate

--- Http/controllers/login/store.php
+++ Http/controllers/login/store.php
@@ -2,27 +2,19 @@
 
 use Core\App;
 use Core\Database;
-use Core\Validator;
+use Http\Forms\LoginForm;
 
 $db = App::resolve(Database::class);
 
 $email = $_POST['email'];
 $password = $_POST['password'];
 
-
-$errors = [];
-if (!Validator::email($email)) {
-    $errors['email'] = 'Please provide a valid email address.';
-}
-
-if (!Validator::string($password)) {
-    $errors['password'] = 'Please provide a valid password.';
-}
-
-if (! empty($errors)) {
+$form = new LoginForm();
+if (!$form->validate($email, $password)) {
     return view('login/create.view.php', [
-        'errors' => $errors
+        'errors' => $form->errors()
     ]);
+
 }
 
 $user = $db->query('SELECT * FROM php_for_beginners.users WHERE email = :email', [
